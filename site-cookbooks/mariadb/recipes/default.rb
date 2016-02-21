7ae5e9f00ba8040958b56135521126fd5be176aa
devtopia <devsoul@icloud.com>
2016-02-21 22:33:26 +0900

    Add cookbooks about yum-update and mariadb

---
+++ site-cookbooks/mariadb/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: mariadb
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file '/etc/yum.repos.d/MariaDB.repo' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+%w(MariaDB-server MariaDB-client).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
