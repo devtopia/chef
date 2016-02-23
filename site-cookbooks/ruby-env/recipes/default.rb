413a2bcaeba78b02d5fca96ff3563c00a5d066f9
devtopia <devsoul@icloud.com>
2016-02-23 19:18:20 +0900

    Add ruby-build and install ruby 2.3.0

--- site-cookbooks/ruby-env/recipes/default.rb
+++ site-cookbooks/ruby-env/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w(gcc git openssl-devel).each do |pkg|
+%w(gcc git openssl-devel readline-devel).each do |pkg|
   package pkg do
     action :install
   end
@@ -27,4 +27,26 @@ template ".bash_profile" do
   owner node['ruby-env']['user']
   group node['ruby-env']['group']
   not_if "grep rbenv ~/.bash_profile", :environment => {:'HOME' => "/home/#{node['ruby-env']['user']}"}
+end
+
+directory "/home/#{node['ruby-env']['user']}/.rbenv/plugins" do
+  owner node['ruby-env']['user']
+  group node['ruby-env']['group']
+  mode '0755'
+  action :create
+end
+
+git "/home/#{node['ruby-env']['user']}/.rbenv/plugins/ruby-build" do
+  repository node['ruby-env']['ruby-build_url']
+  action :sync
+  user node['ruby-env']['user']
+  group node['ruby-env']['group']
+end
+
+execute "rbenv install #{node['ruby-env']['version']}" do
+  command "/home/#{node['ruby-env']['user']}/.rbenv/bin/rbenv install #{node['ruby-env']['version']}"
+  user node['ruby-env']['user']
+  group node['ruby-env']['group']
+  environment 'HOME' => "/home/#{node['ruby-env']['user']}"
+  not_if { File.exists?("/home/#{node['ruby-env']['user']}/.rbenv/versions/#{node['ruby-env']['version']}") }
 end
